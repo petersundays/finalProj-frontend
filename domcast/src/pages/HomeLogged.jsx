396298cd3952710abc2c75685458d25d8589db8d
JoseSousaCastro <152998913+JoseSousaCastro@users.noreply.github.com>
2024-06-25 17:55:39 +0100

    renaming02

--- domcast/src/pages/HomeLogged.jsx
+++ domcast/src/pages/HomeLogged.jsx
@@ -8,23 +8,23 @@ import MainOffcanvasLogged from "../components/MainOffcanvasLogged/MainOffcanvas
 import MainSidebar from "../components/MainSidebar/MainSidebar";
 import MainFooter from "../components/MainFooter/MainFooter";
 import MessageHub from "../components/MessageHub/MessageHub";
-import NewAsset from "../components/AssetNew/AssetNew";
-import CardGrid from "../components/ProjectsListLogged/ProjectsListLogged";
-import NewProj from "../components/ProjectNew/ProjectNew";
-import NewTask from "../components/TaskNew/TaskNew";
+import AssetNew from "../components/AssetNew/AssetNew";
+import ProjectsListLogged from "../components/ProjectsListLogged/ProjectsListLogged";
+import ProjectNew from "../components/ProjectNew/ProjectNew";
+import TaskNew from "../components/TaskNew/TaskNew";
 import AdminSettings from "../components/AdminSettings/AdminSettings";
-import EditAsset from "../components/AssetEdit/AssetEdit";
-import EditProj from "../components/ProjectEdit/ProjectEdit";
-import EditTask from "../components/TaskEdit/TaskEdit";
-import EditProfile from "../components/UserEditProfile/UserEditProfile";
-import GanttChart from "../components/TasksListGantt/TasksListGantt";
+import AssetEdit from "../components/AssetEdit/AssetEdit";
+import ProjectEdit from "../components/ProjectEdit/ProjectEdit";
+import TaskEdit from "../components/TaskEdit/TaskEdit";
+import UserEditProfile from "../components/UserEditProfile/UserEditProfile";
+import TasksListGantt from "../components/TasksListGantt/TasksListGantt";
 import ProjPublic from "../components/ProjectPublic/ProjPublic";
 import ProjPrivate from "../components/ProjectPrivate/ProjPrivate";
 import AdminStats from "../components/AdminStats/AdminStats";
 //import UsersList from "../components/UsersList/UsersList";
-import ViewProfile from "../components/UserViewProfilePublic/UserViewProfilePublic";
-import ViewAsset from "../components/AssetView/AssetView";
-import ViewTask from "../components/TaskView/TaskView";
+import UserViewProfilePublic from "../components/UserViewProfilePublic/UserViewProfilePublic";
+import AssetView from "../components/AssetView/AssetView";
+import TaskView from "../components/TaskView/TaskView";
 
 function HomeLogged() {
   const [show, setShow] = useState(false);
@@ -70,17 +70,17 @@ function HomeLogged() {
               <Card className="custom-card">
                 <Card.Body className="card-body-custom">
                   <Routes>
-                    <Route path="/" element={<CardGrid />} />
+                    <Route path="/" element={<ProjectsListLogged />} />
                     <Route path="/message-hub" element={<MessageHub />} />
-                    <Route path="/add-asset" element={<NewAsset />} />
-                    <Route path="/new-project" element={<NewProj />} />
-                    <Route path="/new-task" element={<NewTask />} />
+                    <Route path="/add-asset" element={<AssetNew />} />
+                    <Route path="/new-project" element={<ProjectNew />} />
+                    <Route path="/new-task" element={<TaskNew />} />
                     <Route path="/settings" element={<AdminSettings />} />
-                    <Route path="/edit-asset/:id" element={<EditAsset />} />
-                    <Route path="/edit-project/:id" element={<EditProj />} />
-                    <Route path="/edit-task/:id" element={<EditTask />} />
-                    <Route path="/edit-profile/:id" element={<EditProfile />} />
-                    <Route path="/project-plan/:id" element={<GanttChart />} />
+                    <Route path="/edit-asset/:id" element={<AssetEdit />} />
+                    <Route path="/edit-project/:id" element={<ProjectEdit />} />
+                    <Route path="/edit-task/:id" element={<TaskEdit />} />
+                    <Route path="/edit-profile/:id" element={<UserEditProfile />} />
+                    <Route path="/project-plan/:id" element={<TasksListGantt />} />
                     <Route path="/project/:id" element={<ProjPublic />} />
                     <Route
                       path="/project-details/:id"
@@ -88,9 +88,9 @@ function HomeLogged() {
                     />
                     <Route path="/statistics" element={<AdminStats />} />
                     {/* <Route path="/users" element={<UsersList />} /> */}
-                    <Route path="/user-profile/:id" element={<ViewProfile />} />
-                    <Route path="/asset/:id" element={<ViewAsset />} />
-                    <Route path="/task/:id" element={<ViewTask />} />
+                    <Route path="/user-profile/:id" element={<UserViewProfilePublic />} />
+                    <Route path="/asset/:id" element={<AssetView />} />
+                    <Route path="/task/:id" element={<TaskView />} />
                   </Routes>
                 </Card.Body>
               </Card>
